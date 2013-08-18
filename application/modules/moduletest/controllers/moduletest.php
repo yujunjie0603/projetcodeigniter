2f5d6da807097c658620bce9621f0669884b9261
yujunjie0603 <yu.long.fr@gmail.com>
2013-08-18 18:46:44 +0200

    itest

---
+++ application/modules/moduletest/controllers/moduletest.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class moduletest extends MX_Controller {
+
+	public function index()
+	{
+		echo "OK, ca marche ! ";
+	}
+}
