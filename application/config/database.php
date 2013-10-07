d15b90b72a2e184dc65cb270f8cf5b9d2cb6627c
yujunjie0603 <yu.long.fr@gmail.com>
2013-10-07 09:10:02 +0200

    test

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'yujunjie830405';
+$db['default']['database'] = 'projet_code_igniter';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
